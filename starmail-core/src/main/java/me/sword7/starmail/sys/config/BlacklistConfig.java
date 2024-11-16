bb516092680e65035fe3e5c67dbdb26b9574e449
SadShrimpy <123626018+SadShrimpyy@users.noreply.github.com>
2024-11-16 21:58:23 +0100

    Refactor blacklist exception handling methods
    Renamed methods and variables for clarity and consistency in `BlacklistConfig`. Added suppression annotations for unused and potential stack trace printing methods. Enhanced method logic for adding and removing exception lines to ensure reliable file handling.

--- starmail-core/src/main/java/me/sword7/starmail/sys/config/BlacklistConfig.java
+++ starmail-core/src/main/java/me/sword7/starmail/sys/config/BlacklistConfig.java
@@ -15,7 +15,7 @@ public class BlacklistConfig {
     private static final File file = new File("plugins/StarMail", "blacklist.yml");
     private static FileConfiguration config = YamlConfiguration.loadConfiguration(file);
 
-    private static String blacklistString = "Items Blacklisted";
+    private static final String blacklistString = "Items Blacklisted";
     private static List<String> blacklist = new ArrayList<>();
 
     public BlacklistConfig() {
@@ -27,10 +27,12 @@ public class BlacklistConfig {
         blacklist = config.getStringList(blacklistString);
     }
 
+    @SuppressWarnings("unused")
     public static Path getFilePath() {
         return file.toPath();
     }
 
+    @SuppressWarnings("CallToPrintStackTrace")
     private void load() {
         if (file.exists()) {
             try {
@@ -41,14 +43,14 @@ public class BlacklistConfig {
         }
     }
 
-    public static void addExceptLine(String exception) {
+    public static void addExceptLine(String requestedException) {
         try {
             final List<String> lines = Files.readAllLines(file.toPath());
             FileWriter fw = new FileWriter(file);
             for (String l : lines) {
                 fw.append(l).append("\n");
             }
-            fw.append("  - ").append(String.valueOf(exception));
+            fw.append("  - ").append(String.valueOf(requestedException));
             fw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -57,19 +59,16 @@ public class BlacklistConfig {
         blacklist = config.getStringList(blacklistString);
     }
 
-    public static void removeHashCode(String exception) {
+    public static void removeExceptLine(String requestedException) {
         try {
             final List<String> lines = Files.readAllLines(file.toPath());
             FileWriter fw = new FileWriter(file);
-            int lineNumber = 0;
-            for (int index = 0; index < lines.size(); index++) {
-                if (lines.get(index).contains(exception)) {
-                    lineNumber = index;
+            for (String l : lines) {
+                System.out.println(l);
+                if (l.equals("  - " + requestedException)) {
+                    continue;
                 }
-            }
-            lines.remove(lineNumber);
-            for (String line : lines) {
-                fw.append(line).append("\n");
+                fw.append(l).append("\n");
             }
             fw.close();
         } catch (IOException e) {
